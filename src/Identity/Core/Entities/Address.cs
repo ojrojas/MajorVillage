61a8e19fdcc3365830828db65d3c8958938d6498
Oscar Rojas <orojasgarces1@gmail.com>
2022-12-02 13:26:13 -0500

    create test projects identity and school, delete address and contact from repo and services

--- src/Identity/Core/Entities/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Entities
-{
-    public class Address : BaseEntity, IAggregateRoot
-    {
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public SelectedEnum PrincipalAddress { get; set; }
-    }
-}
