0344040900f8ccfbf64930b2f28e2e394fa306da
Oscar Rojas <orojasgarces1@gmail.com>
2023-06-14 16:59:55 -0500

    remove main file error

---
+++ src/School/School.Api/Endpoints/Enrollment/GetAllEnrollments.cs
@@ -0,0 +1,27 @@
+﻿namespace School.Api.Endpoints.Enrollment;
+
+[Authorize]
+[Route("api/[controller]")]
+public class GetAllEnrollments : EndpointBaseAsync.WithRequest<GetAllEnrollmentRequest>.WithResult<GetAllEnrollmentResponse>
+{
+
+    private readonly IEnrollmentService _service;
+
+    public GetAllEnrollments(IEnrollmentService service)
+    {
+        _service = service ?? throw new ArgumentNullException(nameof(service));
+    }
+
+    [HttpGet]
+    [Produces(typeof(GetAllEnrollmentResponse))]
+    [SwaggerOperation(
+     Summary = "Get all enrollmens",
+     Description = "Get all enrollments",
+     OperationId = "Enrollment.getallenrollments",
+     Tags = new[] { "EnrollmentEndpoints" })]
+    public override async Task<GetAllEnrollmentResponse> HandleAsync(GetAllEnrollmentRequest request, CancellationToken cancellationToken = default)
+    {
+        return await _service.GetAllEnrollmentsAsync(request, cancellationToken);
+    }
+}
+
--- src/School/School.Core/Repositories/CourseRepository.cs
+++ src/School/School.Core/Repositories/CourseRepository.cs
@@ -2,12 +2,8 @@
 
 public class CourseRepository: GenericRepository<Course>
 {
-	private readonly SchoolDbContext _context;
-	private readonly ILogger<CourseRepository> _logger;
-
-    public CourseRepository(ILogger<CourseRepository> logger,
-                            DbContext context,
-                            ISpecificationEvaluator specificationEvaluator) : base(logger, context, specificationEvaluator)
+	public CourseRepository(ILogger<CourseRepository> logger,
+                            SchoolDbContext context) : base(logger, context)
     {
     }
 }
