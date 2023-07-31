d728166260eb0cc58bbeb34e2b34a54eebf08570
Oscar Rojas <orojasgarces1@gmail.com>
2023-07-31 18:09:13 -0500

    fixing injection dependencies

--- src/School/School.Api/DI/DIServicesApplication.cs
+++ src/School/School.Api/DI/DIServicesApplication.cs
@@ -4,15 +4,21 @@ internal static class DIServicesApplication
 {
     internal static IServiceCollection AddServicesDIApplication(this IServiceCollection services)
     {
-        services.AddTransient(typeof(GenericRepository<>));
+        //services.AddTransient(typeof(GenericRepository<>));
 
         services.AddTransient(typeof(CourseRepository));
         services.AddTransient(typeof(ElectiveYearRepository));
         services.AddTransient(typeof(EnrollmentRepository));
+        services.AddTransient(typeof(ClassRoomRepository));
+        services.AddTransient(typeof(AreaRepository));
+
+        services.AddTransient(typeof(ILoggerApplicationService<>), typeof(LoggerApplicationService<>));
 
         services.AddTransient<ICourseService, CourseService>();
         services.AddTransient<IEnrollmentService, EnrollmentService>();
         services.AddTransient<IElectiveYearService, ElectiveYearService>();
+        services.AddTransient<IClassRoomService, ClassRoomService>();
+        services.AddTransient<IAreaService, AreaService>();
 
         services.AddTransient<InitializerDbContext>();
         return services;
