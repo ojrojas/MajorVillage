5581713bbd6ac0cfdf3b838ef9a8710d7689c7ed
Oscar Rojas <orojasgarces1@gmail.com>
2023-08-08 14:50:53 -0500

    create subject, student, period

---
+++ src/School/School.Core/Dtos/Student/StudenDto.cs
@@ -0,0 +1,53 @@
+﻿namespace School.Core.Dtos;
+
+public record CreateStudentRequest: BaseRequest
+{
+    public Student Student { get; set; }
+}
+
+public record CreateStudentResponse: BaseResponse
+{
+    public CreateStudentResponse(Guid correlationId): base(correlationId)  { }
+    public Student StudentCreated { get; set; }
+}
+
+public record UpdateStudentRequest: BaseRequest
+{
+    public Student Student { get; set; }
+}
+
+public record UpdateStudentResponse: BaseResponse
+{
+    public UpdateStudentResponse(Guid correlationId): base(correlationId) {}
+    public Student StudentUpdated { get; set; }
+}
+
+public record DeleteStudentRequest: BaseRequest
+{
+    public Guid Id { get; set; }
+}
+
+public record DeleteStudentResponse: BaseResponse
+{
+    public DeleteStudentResponse(Guid correlationId): base(correlationId) { }
+    public bool IsStudentDeleted { get; set; }
+}
+
+public record GetStudentByIdRequest: BaseRequest
+{
+    public Guid Id { get; set; }
+}
+
+public record GetStudentByIdResponse: BaseResponse
+{
+    public GetStudentByIdResponse(Guid correlationId): base(correlationId) { }
+    public Student Student { get; set; }
+}
+
+public record GetAllStudentsRequest : BaseRequest { }
+
+public record GetAllStudentsResponse : BaseResponse
+{
+    public GetAllStudentsResponse(Guid correlationId): base(correlationId) { }
+    public IEnumerable<Student> Students { get; set; }
+}
--- src/School/School.Core/Services/StudentService.cs
+++ src/School/School.Core/Services/StudentService.cs
@@ -1,11 +1,65 @@
-﻿using System;
-namespace School.Core.Services
+﻿namespace School.Core.Services;
+
+public class StudentService : IStudentService
 {
-	public class StudentService
-	{
-		public StudentService()
-		{
-		}
-	}
+    private readonly StudentRepository _repository;
+    private readonly ILoggerApplicationService<StudentService> _logger;
+
+    public StudentService(StudentRepository repository, ILoggerApplicationService<StudentService> logger)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async ValueTask<CreateStudentResponse> CreateStudentAsync(CreateStudentRequest request, CancellationToken cancellationToken)
+    {
+        CreateStudentResponse response = new(request.CorrelationId());
+        _logger.LogInformation(response, "Create Student request");
+        response.StudentCreated = await _repository.CreateAsync(request.Student, cancellationToken);
+        _logger.LogInformation($"Created record params {JsonSerializer.Serialize(request.Student)}");
+
+        return response;
+    }
+
+    public async ValueTask<UpdateStudentResponse> UpdateStudentAsync(UpdateStudentRequest request, CancellationToken cancellationToken)
+    {
+        UpdateStudentResponse response = new UpdateStudentResponse(request.CorrelationId());
+        _logger.LogInformation(response, "Update Student request");
+        response.StudentUpdated = await _repository.UpdateAsync(request.Student, cancellationToken);
+        _logger.LogInformation($"Update record params {JsonSerializer.Serialize(request.Student)}");
+
+        return response;
+    }
+
+    public async ValueTask<DeleteStudentResponse> DeleteStudentAsync(DeleteStudentRequest request, CancellationToken cancellationToken)
+    {
+        DeleteStudentResponse response = new(request.CorrelationId());
+        _logger.LogInformation(response, "Delete Student request");
+        var StudentToDelete = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        response.IsStudentDeleted = await _repository.DeleteAsync(StudentToDelete, cancellationToken) is not null;
+        _logger.LogInformation($"Delete record params {request.Id}");
+
+        return response;
+    }
+
+    public async ValueTask<GetStudentByIdResponse> GetStudentByIdAsync(GetStudentByIdRequest request, CancellationToken cancellationToken)
+    {
+        GetStudentByIdResponse response = new GetStudentByIdResponse(request.CorrelationId());
+        _logger.LogInformation(response, "Get Student by id request");
+        response.Student = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        _logger.LogInformation($"Get by id Student params {request.Id}");
+
+        return response;
+    }
+
+    public async ValueTask<GetAllStudentsResponse> GetAllStudentsAsync(GetAllStudentsRequest request, CancellationToken cancellationToken)
+    {
+        GetAllStudentsResponse response = new(request.CorrelationId());
+        _logger.LogInformation(response, "Get all Students request");
+        response.Students = await _repository.ListAsync(cancellationToken);
+        _logger.LogInformation($"Get all Students response count: {response.Students.Count()}");
+
+        return response;
+    }
 }
 
